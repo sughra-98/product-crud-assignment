7f82294c1f278d5e5c15397c201a6ebfd42a31f5
sughra-98 <66856660+sughra-98@users.noreply.github.com>
2025-07-28 11:53:27 +0300

    Update Product.java

--- product-crud-assignment/src/main/java/com/example/product_crud_assignment/Product.java
+++ product-crud-assignment/src/main/java/com/example/product_crud_assignment/Product.java
@@ -38,11 +38,11 @@ import jakarta.persistence.Table;
            this.quantity = quantity;
        }
 
-       public long getId() {
+       public Long getId() {
            return id;
        }
 
-       public void setId(long id) {
+       public void setId(Long id) {
            this.id = id;
        }
 
